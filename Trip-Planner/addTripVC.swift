e4ee2d2465511837957dfe59a6187b0585f3bcef
Phyllis Wong <wong.phyllism@gmail.com>
2017-12-11 10:46:16 -0800

    formatt elements in the add trip view

--- Trip-Planner/addTripVC.swift
+++ Trip-Planner/addTripVC.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class addTripVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var destinationTF: UITextField!
+    @IBOutlet weak var waypointsTF: UITextField!
+    @IBOutlet weak var completedTF: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    // When the user presses this button, it saves the trip.
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
