0763f1a3e05e6f801f96b73dff5ceb90a06adc27
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-08 20:20:52 -0800

    add keychainSwift library

--- Trip-Planner/LoginVC.swift
+++ Trip-Planner/LoginVC.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import KeychainSwift
+
 
 class LoginVC: UIViewController {
+    
+    let keychain = KeychainSwift()
+    
     
     // Variables
     // Perminently store user data
@@ -32,12 +37,17 @@ class LoginVC: UIViewController {
     @IBAction func loginBtn(_ sender: Any) {
         let user = User(email: emailTF.text!, username: usernameTF.text!, password: passwordTF.text!)
         print(user)
+        keychain.set(user.username, forKey: "username")
+        keychain.set(user.password, forKey: "password")
     }
     @IBAction func signupBtn(_ sender: Any) {
         let user = User(email: emailTF.text!, username: usernameTF.text!, password: passwordTF.text!)
         
         Networking.fetch(route: Route.users(), user: user, httpMethod: HTTPMethod.get) { (data, int) in
             if int == 200 {
+                let username = self.keychain.get("username")
+                let password = self.keychain.get("password")
+                print(username, password)
                 UserDefaults.standard.set(true, forKey: "isUserLoggedIn")
                 let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
                 let tripsVC = storyboard.instantiateViewController(withIdentifier: "TripsVC") as! TripsVC
@@ -60,8 +70,8 @@ extension LoginVC: UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
 }
-
 
 
 
